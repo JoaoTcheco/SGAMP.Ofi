709032291ade32abbbfa701f8b8700b63d07f297
fernandonkuna <127996075+fernandonkuna@users.noreply.github.com>
2025-05-28 14:50:23 +0100

    Class Service

---
+++ src/main/java/com/engsoft/sgamp/service/ConsultaService.java
@@ -0,0 +1,36 @@
+package com.engsoft.sgamp.service;
+
+import com.engsoft.sgamp.model.Consulta;
+import com.engsoft.sgamp.repository.ConsultaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class ConsultaService {
+
+    @Autowired
+    private ConsultaRepository consultaRepository;
+
+    public List<Consulta> listarTodas() {
+        return consultaRepository.findAll();
+    }
+
+    public Optional<Consulta> buscarPorId(Long id) {
+        return consultaRepository.findById(id);
+    }
+
+    public Consulta salvar(Consulta consulta) {
+        return consultaRepository.save(consulta);
+    }
+
+    public void deletar(Long id) {
+        consultaRepository.deleteById(id);
+    }
+
+    public List<Consulta> listarPorPacienteId(Long pacienteId) {
+        return consultaRepository.findByPacienteId(pacienteId);
+    }
+}
