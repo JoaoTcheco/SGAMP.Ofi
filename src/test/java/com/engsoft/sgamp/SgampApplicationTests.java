3c526aca4e5530cfb1d99925a49b9b25df2e52af
JoaoTcheco <asagas153@gmail.com>
2025-05-28 21:20:36 +0000

    reciclagem
    reclicagem

---
+++ src/test/java/com/engsoft/sgamp/SgampApplicationTests.java
@@ -0,0 +1,13 @@
+package com.engsoft.sgamp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SgampApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
